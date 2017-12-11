8c59c73038383fe84b56caafd71700fe3c09dc27
zxw <Zxw854953678@163.com>
2017-12-12 03:21:30 +0800

    modify app.js and package.json

--- app.js
+++ app.js
@@ -2,13 +2,17 @@
  * Created by ZhouXiaoWen on 17-12-9.
  */
 
-const express = require("express");
-
 const path = require("path");
+const express = require("express");
 const bodyParser = require('body-parser');
-
-
-// console.log(path.join(__dirname, 'views'));
+const serveStatic = require('serve-static');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+const logger = require('morgan');
+const config = require('./config/config.js');
 
 const app = express();
 
@@ -16,11 +20,33 @@ const app = express();
 app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'hbs');
 
-// 指定静态资源文件
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(serveStatic(path.join(__dirname, 'public')));
+app.use("/", bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(session({
+  // cookie中用来保存session id 的字段名
+  name: 'connect.sid',
+  // 通过设置secret来计算hash值并且存在cookie中, 产生的signedCookie放篡改
+  secret: 'niit',
+  // 每次请求都重新设置session id的cookie的过期时间
+  resave: true,
+  // true 则 无论有没有session id 的cookie, 每次请求都要重新设置session cookie
+  saveUninitialized: false,
+  store: new MongoStore({
+    url: config.dbUrl, // 保存session使用的mongoDB的url
+    collection: 'sessions', // 指定保存session的表名
+    auto_reconnect: true //自动连接数据库
+  })
+}));
 
-//bodyParser
-app.use("/", bodyParser.urlencoded({extended: false}));
+const env = 'development';
+// console.log(`------> current env ${env} <------`);
+if ('development' === env) {
+  app.set('showStackError', true);
+  // 使用morgan日志中间件 指定日志的输出格式
+  app.use(logger(':method :url :status'));
+  mongoose.set('debug', true);
+}
 
 app.get("/", (req, res)=> {
   "use strict";
@@ -55,7 +81,7 @@ app.post("/student", (req, res)=> {
 app.get("/signUp", (req, res)=> {
   "use strict";
   const data = {};
-  res.render("signUp",data);
+  res.render("signUp", data);
 });
 app.post("/signUp", (req, res)=> {
   "use strict";
@@ -69,7 +95,6 @@ app.post("/signUp", (req, res)=> {
   res.redirect("/signUp");
 
 });
-
 app.listen(3000, ()=> {
   "use strict";
   console.log("server start...")
