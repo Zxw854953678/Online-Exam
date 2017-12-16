f2be423a227e80671910dd350addba9d4e6d3fce
zxw <Zxw854953678@163.com>
2017-12-17 02:38:12 +0800

    add models teacher

--- app/models/teacher.js
+++ app/models/teacher.js
@@ -4,35 +4,35 @@
 
 const mongoose = require('mongoose');
 const path = require('path');
-const teacherSchema = require(path.join(__dirname, '../schemas/teacherSchema'));
-const Teacher = mongoose.model('teachers', teacherSchema);
+const TeacherSchema = require(path.join(__dirname, '../schemas/teacherSchema'));
+const Teacher = mongoose.model('teachers', TeacherSchema);
 
 
 // -----------------------test----------------------------------
-Teacher.findOne({sno: "S201701001"}, function (err, student) {
-  if (err) {
-    throw err;
-  }
-  console.log(student);
-});
+// Teacher.findOne({sno: "S201701001"}, function (err, student) {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log(student);
+// });
 
-const teacher = new Teacher({
-  tno: "9088989",
-  pwd: "123123",
-  personalInfo: {
-    name: "Dan",
-    course: "html",
-    sex: "male",
-    birth: "1979/12/12",
-    email: "3233ooo2@126.com"
-  },
-  role:1,
-  meta: {
-    createAt: new Date(),
-    updateAt: new Date()
-  }
-});
-teacher.save();
+// const teacher = new Teacher({
+//   tno: "T123456",
+//   pwd: "123123",
+//   personalInfo: {
+//     name: "Dan",
+//     course: "html",
+//     sex: "male",
+//     birth: "1979/12/12",
+//     email: "3233ooo2@126.com"
+//   },
+//   role:1,
+//   meta: {
+//     createAt: new Date(),
+//     updateAt: new Date()
+//   }
+// });
+// teacher.save();
 //------------------------------------------------------------
 
 module.exports = Teacher;
